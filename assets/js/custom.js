93254797986a83404950dea3694547b831913150
Rasel Ahsan <raselahsan414@gmail.com>
2020-11-16 11:06:53 +0600

    up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -76,3 +76,69 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+/*
+    Carousel
+*/
+var breakpoint = {
+  // Extra Small screen / phone
+  xs: 300,
+  // Small screen / phone
+  sm: 320,
+  // Medium screen / tablet
+  md: 768,
+  // Large screen / desktop
+  lg: 992,
+  // Extra large screen / wide desktop
+  xl: 1200
+};
+
+// slick slider
+$('#slick').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  draggable: true,
+  infinite: true,
+  dots: false,
+  arrows: false,
+  speed: 1000,
+  mobileFirst: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [{
+      breakpoint: breakpoint.xs,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.sm,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.md,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.lg,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: breakpoint.xl,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
