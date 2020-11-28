121dc75f77233f3bb4d9e04281f9145cd009012e
Rasel Ahsan <raselahsan414@gmail.com>
2020-11-28 17:50:17 +0600

    up

--- assets/js/template2.js
+++ assets/js/template2.js
@@ -20,3 +20,68 @@ $('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
       next.children(':first-child').clone().appendTo($(this));
     }
   });
+
+  /*
+    Carousel
+*/
+var breakpoint = {
+  // Extra Small screen / phone
+  xs: 300,
+  // Small screen / phone
+  sm: 320,
+  // Medium screen / tablet
+  md: 768,
+  // Large screen / desktop
+  lg: 992,
+  // Extra large screen / wide desktop
+  xl: 1200
+};
+  // slick slider
+$('#slick').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    draggable: true,
+    infinite: true,
+    dots: false,
+    arrows: false,
+    speed: 1000,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [{
+        breakpoint: breakpoint.xs,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: breakpoint.sm,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: breakpoint.md,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: breakpoint.lg,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: breakpoint.xl,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
